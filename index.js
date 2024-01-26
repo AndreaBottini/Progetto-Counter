3d882eb83d040c1941f2c6f5adc8dea955b7b261
Andrea Bottini <andrea.bottini22@gmail.com>
2024-01-26 15:31:54 +0100

    Second Commit and end

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// scrivi qui il tuo codice
+//impostazione counter a 0
+let counterNumber = 0;
+
+//selezionare i bottoni e il counter
+const count = document.getElementById("count");
+console.log(count)
+const buttonsToAddAndRemove = document.querySelectorAll(".button-xs");
+console.log(buttonsToAddAndRemove);
+
+buttonsToAddAndRemove.forEach(button => {
+    button.addEventListener("click", function(e){
+        //console.log(button)
+        //console.log(e.currentTarget.classList);
+        const style = e.currentTarget.classList;
+        if(style.contains("minus")){
+            counterNumber--;
+        } else if(style.contains("plus")) {
+            counterNumber++;
+        } else {
+            counterNumber = 0;
+        }
+
+        if(counterNumber > 0) {
+            count.style.color = "green";
+        } else if (counterNumber < 0) {
+            count.style.color = "red";
+        } else {
+            count.style.color = "white"
+        }
+
+        count.textContent = counterNumber; 
+    })
+});
+
+
+
